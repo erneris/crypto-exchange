f11d4ccfbcdedb4237d1b54190489c66c2c377ed
New User <newuser@example.com>
2024-06-03 19:05:07 +0300

    added basis for profile system

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+import sys, os, csv, colorama, json
+from termcolor import colored
+DATA_FOLDER_NAME = "data"
 
+
+        
+
+def save_profiles():
+    pass
+
+def get_profiles():
+    try:
+        with open(f"{DATA_FOLDER_NAME}/profiles.json") as file:
+            data = json.load(file)
+            return data["profiles"]
+
+    except FileNotFoundError: #if file isn't created
+        with open(f"{DATA_FOLDER_NAME}/profiles.json", "w") as file:
+            template = {"profiles": []}
+            json.dump(template, file, ensure_ascii=False, indent=4) #arguments for better readability
+            return []
+        
+def select_profile():
+    profiles = get_profiles()
+    print(profiles)
+    if len(profiles) == 0:
+        pass
+        #create_profile(profiles)
+    for profile in profiles:
+        print(profile)
 
 def main():
-    pass
+    clear()
+    colorama.init()
+    select_profile()
+
+def clear():
+    os.system('clear')
 
 if __name__ == "__main__":
     main()
